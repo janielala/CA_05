3be0ad0b61cfa2e55faeca11998257f0776cf6d0
janielala <148871742+janielala@users.noreply.github.com>
2023-11-07 21:13:10 +0800

    Initial commit

---
+++ practice.js
@@ -0,0 +1,36 @@
+// Your Javascript practice exercise goes here
+
+/**
+ * JavaScript Practice Variables / typeof
+ *
+ */
+
+/* 
+JavaScript Practice Exercise
+What is the output of these comparisons
+
+x = 8
+y = 3
+
+x == y
+x && y < 10
+x || y <= 8
+y === '3'
+!(x == 5)
+
+*/
+
+/**
+ * JavaScript Practice Arrays
+ *
+ */
+
+/**
+ * JavaScript Practice Objects
+ *
+ */
+
+/**
+ * JavaScript Array & Object Destructuring
+ *
+ */
